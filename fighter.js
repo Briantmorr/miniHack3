9d258fe2a780e01d15c29394c51878e5b09fe3eb
Li Zhang <bylizhang@gmail.com>
2018-03-01 20:19:33 -0800

    added printMessage fn in fighter.js

--- fighter.js
+++ fighter.js
@@ -1,4 +1,4 @@
-class Figher {
+class Fighter {
 	constructor(fighterName, hp) {
 		this.name = fighterName || 'fighter';
 		this.hp = hp || 100;
@@ -8,25 +8,25 @@ class Figher {
 	}
 
 	attack(target){
-		console.log(`${this.name} attacks ${target.name}.`);
+		printMessage(`${this.name} attacks ${target.name}.`);
 	}
 
 	takeDamage(target){
 		target.hp -= 10;
 		target.turnToAttack = true;
-		console.log(`${this.name} took ${this.baseDamage} damage from ${target.name}.`);
+		printMessage(`${this.name} took ${this.baseDamage} damage from ${target.name}.`);
 	}
 	printMessage(message){
         const $div = $('<div>');
         const $name = $('<span>', {
             text: this.name + ": ",
-            class: 'truck-name'
+            class: 'fighter-name'
         });
         const $msg = $('<span>', {
             text: message,
             class: 'message'
         });
         $div.append($name, $msg);
-        $('#root').append($div);
+        $('.battleLog').append($div);
     }
 }
